62aa01d7a60e1c7d774a6efa9e7f10cb5b55fe83
javier castillo <javier73castillo@hotmail.com>
2022-04-14 19:55:23 +0200

    new update cors

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ configCloudinary();
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH')
     res.header('Access-Control-Allow-Credentials', false)
-    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.header('Access-Control-Allow-Origin', '*')
     next()
 })
