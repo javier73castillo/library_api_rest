d91031203f02a5d9ff52cf6b10f4a25d3dd46b7c
javier castillo <javier73castillo@hotmail.com>
2022-04-16 19:27:35 +0200

    new access control

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH')
     res.header('Access-Control-Allow-Credentials', false)
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin: *')
     next()
 })
 
